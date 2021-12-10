f00de43bec9be02eee57cad57e2707e47cb7e596
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-12-10 18:13:38 +0200

    courses page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Welcome from "./pages/Welcome";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import Courses from "./pages/Courses";
 import "./App.css";
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/signup" element={<Signup />}></Route>
           <Route exact path="/login" element={<Login />}></Route>
           <Route exact path="/profile" element={<Profile />}></Route>
+          <Route exact path="/Courses" element={<Courses />}></Route>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Courses.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+import "./../style/courses.css";
+import cardImg from "./../assets/logo.png";
+class Courses extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div>
+          <h1 className="m-5 p-5">Courses</h1>
+        </div>
+        <div className="card-group justify-content-center align-items-center ">
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+          <div className="card card_de m-3">
+            <img className="card-img-top" src={cardImg} alt="Card image cap" />
+            <div className="card-body">
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <button type="button" className="btn btn-warning">
+                View Course
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Courses;
