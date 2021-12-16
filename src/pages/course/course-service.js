954ed650ae10d8b497a691e09fb7976106317923
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-12-16 08:54:48 +0200

    clean usage of axios

--- src/pages/course/course-service.js
+++ src/pages/course/course-service.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axiosClient from "../../client";
 
-export const getCourseMetadata  = (course_id) => axios.get(`/courses/${course_id}`);
+export const getCourseMetadata  = (course_id) => axiosClient.get(`/courses/${course_id}`);
 
-export const editCourseMetadata = (course_id) => axios.patch(`/courses/${course_id}`);
+export const editCourseMetadata = (course_id) => axiosClient.patch(`/courses/${course_id}`);
 
-export const getCourseProgress  = (course_id) => axios.get(`/courses/${course_id}/progress`);
+export const getCourseProgress  = (course_id) => axiosClient.get(`/courses/${course_id}/progress`);
 
-export const enrollToCourse     = (course_id) => axios.post(`/my/courses/${course_id}/enroll`);
+export const enrollToCourse     = (course_id) => axiosClient.post(`/my/courses/${course_id}/enroll`);
 
-export const getActivity        = (course_id) => axios.get(`/courses/${course_id}/activites`);
+export const getActivity        = (course_id) => axiosClient.get(`/courses/${course_id}/activites`);
 
-export const getActivityContent = (course_id, activity_id) => axios.get(`/courses/${course_id}/activities/${activity_id}/content`);
+export const getActivityContent = (course_id, activity_id) => axiosClient.get(`/courses/${course_id}/activities/${activity_id}/content`);
 
-export const finishActivity     = (course_id, activity_id) => axios.post(`/my/courses/${course_id}/progress/${activity_id}`);
+export const finishActivity     = (course_id, activity_id) => axiosClient.post(`/my/courses/${course_id}/progress/${activity_id}`);
 
-export const addActivity        = (course_id) => axios.post(`/courses/${course_id}/activities`);
+export const addActivity        = (course_id) => axiosClient.post(`/courses/${course_id}/activities`);
