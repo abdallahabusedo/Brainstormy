8e5b46616c6583342c4ff88c9a54e1b71da1f15a
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-12-16 06:04:06 +0200

    Add course service

---
+++ src/pages/course/course-service.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const getCourseMetadata  = (course_id) => axios.get(`/courses/${course_id}`);
+
+export const editCourseMetadata = (course_id) => axios.patch(`/courses/${course_id}`);
+
+export const getCourseProgress  = (course_id) => axios.get(`/courses/${course_id}/progress`);
+
+export const enrollToCourse     = (course_id) => axios.post(`/my/courses/${course_id}/enroll`);
+
+export const getActivity        = (course_id) => axios.get(`/courses/${course_id}/activites`);
+
+export const getActivityContent = (course_id, activity_id) => axios.get(`/courses/${course_id}/activities/${activity_id}/content`);
+
+export const finishActivity     = (course_id, activity_id) => axios.post(`/my/courses/${course_id}/progress/${activity_id}`);
+
+export const addActivity        = (course_id) => axios.post(`/courses/${course_id}/activities`);
