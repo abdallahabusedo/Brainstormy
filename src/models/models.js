4c681441d8054bf1843f9957b94a48308a1ec1e0
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-01-02 08:23:45 +0200

    Adding packages and models

--- src/models/models.js
+++ src/models/models.js
@@ -1,40 +1,63 @@
-import { array, binary, boolean, date, number, object, string } from "joi";
+import Joi from "joi";
 
+export const ADMIN = 1, INSTRUCTOR = 2, LEARNER = 3;
+export const PDF = 0, VIDEO = 1, QUIZ = 2;
+export const ENROLLED = '0' , CREATED = '1';
 
-export const CourseMetadata = object({
-    id: string(),
-    name: string(),
-    start_date: date(),
-    end_date: date(),
-    description: string(),
-    instructor_id: string(),
+export const Instructor = Joi.object({
+    id: Joi.number(),
+    first_name: Joi.string(),
 });
 
-export const CourseProgress = object({
-    total_completeness: number().max(100).min(0),
-    done: object.pattern(string(), boolean()),
+export const UserData = Joi.object({
+    id:         Joi.number(),
+    username:   Joi.string(),
+    first_name: Joi.string(),
+    last_name:  Joi.string(),
+    birthdate:  Joi.date(),
+    email:      Joi.string().email({ tlds: {allow: false} }),
+    type:       Joi.number().allow(ADMIN, INSTRUCTOR, LEARNER),
 });
 
-export const ActivityMetadata = object({
-    id: string(),
-    type: string().allow('video', 'pdf'),
-    title: string(),
-    description: string(),
+export const CourseMetadata = Joi.object({
+    id:             Joi.number(),
+    name:           Joi.string(),
+    start_date:     Joi.date().iso(),
+    end_date:       Joi.date().iso(),
+    description:    Joi.string(),
+    instructor:     [ Joi.number().optional(), Instructor ],
+    users:          Joi.array().items(Joi.number()),
+});
+
+
+export const CourseProgress = Joi.object({
+    total_completeness: Joi.number().max(100).min(0),
+    done:               Joi.object().pattern(Joi.number(), Joi.boolean()),  // Map<string, boolean>
+});
+
+export const ActivityMetadata = Joi.object({
+    id:             Joi.number(),
+    courseId:       Joi.number(),
+    type:            Joi.number().allow(PDF, VIDEO, QUIZ),
+    title:          Joi.string(),
+    description:    Joi.string(),
 });
 
 export const CourseData = CourseMetadata.options({ presence: 'required' }).keys({
-    progress: CourseProgress.optional(),
-    activiies: array().items(ActivityMetadata),
+    progress:   CourseProgress.optional(),
+    activities: Joi.array().items(ActivityMetadata),
 });
 
-export const VideoContent = object({
-    url: string().uri()
+export const VideoContent = Joi.object({
+    url: Joi.string().uri()
 });
 
-export const PDFContent = binary();
+export const PDFContent = Joi.object({
+    file: Joi.string().base64()
+});
 
-export const ActivityContent = [VideoContent, PDFContent];
+export const ActivityContentModel = [VideoContent, PDFContent];
 
 export const ActivityData = ActivityMetadata.keys({
-    content: ActivityContent
+    content: ActivityContentModel
 });
