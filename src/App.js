92db9cd473352530514d0bc2e1c45fc516965041
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2022-01-02 10:15:53 +0200

    Add course page and route

--- src/App.js
+++ src/App.js
@@ -6,23 +6,25 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Courses from "./pages/Courses";
-import CoursePage from "./pages/CoursePage";
+import CoursePage from "./pages/course/CoursePage";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Welcome />}></Route>
-          <Route exact path="/signup" element={<Signup />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/profile" element={<Profile />}></Route>
-          <Route exact path="/Courses" element={<Courses />}></Route>
-          <Route exact path="/course/:id" element={<CoursePage />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route exact path="/"           element={<Welcome     />}></Route>
+            <Route exact path="/signup"     element={<Signup      />}></Route>
+            <Route exact path="/login"      element={<Login       />}></Route>
+            <Route exact path="/profile"    element={<Profile     />}></Route>
+            <Route exact path="/courses"    element={<Courses     />}></Route>
+            <Route exact path="/course/:id" element={<CoursePage  />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/pages/course/CoursePage.jsx
+++ src/pages/course/CoursePage.jsx
@@ -1,113 +1,87 @@
-import React, { Component } from "react";
-import Header from "../components/Header";
-import courseImg from "./../assets/logo.png";
-import "./../../src/style/courses.css";
+import React, { useState, useEffect, useContext } from "react";
+import Header from "../../components/Header";
+import "../../../src/style/courses.css";
+import { getCourseAll } from "../../services/course-service";
+import { CourseData } from "../../models/models";
+import { logError } from "../../common/logger";
+import CourseInfo from "./CourseInfo";
+import Content from "./content/Content";
+import Progress from "./Progress";
+import CourseDataProvider, { CourseDataContext } from "../../providers/CourseDataProvider";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import ErrorViewer from "../../components/Error";
 
-import { getCourseAll } from "./course-service";
-import { CourseMetadata } from "../../models/models";
-import { error, logError } from "../../logger";
-import { getRouteParam } from "../../common/helpers";
-export default class CoursePage extends Component {
-  constructor(props) {
-    super(props);
-    console.log(getRouteParam(2));
-    this.state = {
-      id: getRouteParam(2),
-      name: '',
-      start_date: new Date(),
-      end_date: new Date(),
-      description: '',
-      instructor_id: '',
-      activities: [],
-      progress: {},
-    };
-  }
-  componentDidMount = async () => {
-    try {
-      // Get course metadata
-      const res = await getCourseAll(this.state.id);
-      
-      // Validate
-      const { value, error } = CourseMetadata.validate(res.data, { stripUnknown: true, presence: 'required'});
+toast.configure();
 
-      console.log(validCourseMetadata.value);
+function Course(props) {
+  const [ course, setCourse ] = useContext(CourseDataContext);
+  const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState('');
 
-      // Set state
-      this.setState(validCourseMetadata.value);
+  useEffect(() => {
+    (async () => {
+      try {
+        // Get course metadata
+        const res = await getCourseAll(course.id);
+        
+        // Validate and cast
+        const validCourseData = await CourseData.validateAsync(res.data, { stripUnknown: true, presence: 'required'});
+        
+        console.log(validCourseData);
 
-      // TODO :: Remove
-      console.log(this.state);
-    
-    } catch (e) {
-      logError(e);
-    }
-  };
-  render() {
-    return (
-      <div>
-        <Header />
-        <div>
-          <div>
-            <section className="vh-100 mt-5">
-              <div className="container py-5 h-100">
-                <div className="row d-flex justify-content-center align-items-center h-100">
-                  <div className="col-md-0 col-xl-8">
-                    <div className="card">
-                      <div className="card-body text-center">
-                        {this.CourseInfo()}
-                        {this.Enrol()}
-                        <h2 className="h2">content</h2>
-                        {this.Content()}
-                        {this.state.files}
-                      </div>
-                    </div>
-                  </div>
+        // Set state
+        setCourse(validCourseData);
+
+        setLoaded(true);
+
+      } catch (e) {
+        console.log(e);
+        setError(e);
+        logError(e.message);
+      }
+    })();
+    return () => {}
+  }, []);
+
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Header />
+        <section className="vh-100 mt-5 py-5">
+          {
+            loaded ? (
+              <div className="row d-flex">
+                <div className="col-12">
+                  <CourseInfo/>
+                  <Progress />
+                  <Content />
                 </div>
               </div>
-            </section>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  Content() {
-    return (
-      <div className="file-input  btn-lg ">
-        <input
-          type="file"
-          id="file"
-          className="file"
-          onClick={(e) => this.onChange(e)}
-        />
-        <label for="file">Select file</label>
-      </div>
-    );
-  }
+            ) : (
+              <ErrorViewer backLink={'/courses'} />
+            )
+          }
+        </section>  
+    </>
+  )
+}
 
-  Enrol() {
-    return (
-      <div>
-        <button className="btn btn-warning btn-lg btn-block mt-3 mb-5">
-          Enrol
-        </button>
-      </div>
-    );
-  }
-  CourseInfo() {
-    return (
-      <div className="mt-0 mb-0">
-        <div>
-          <img
-            src={courseImg}
-            className="rounded-circle img-fluid"
-            width="200px"
-          />
-        </div>
-        <h4 className="mb-2">{this.state.courses.name}</h4>
-
-        <h4 className="mb-2">{this.state.courses.instructor}</h4>
-        <p className=" h5 card-text">{this.state.courses.description}</p>
-      </div>
-    );
-  }
+export default function CoursePage() {
+  return (
+    <CourseDataProvider>
+      <Course />
+    </CourseDataProvider>
+  )
 }
